b53dda75b6a8cc401b48e99dfc0da69e1d74e6d4
Johnny <johnny.liu@ausy.be>
2021-02-01 14:43:37 +0100

    Routes & Controllers
    Routes + routes via annotations

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage() {
+        return new Response('What a nice controller we have conjured!');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug) {
+        return new Response(sprintf(
+            'Future page to show a question "%s"!',
+            str_replace('-', ' ', $slug)
+        ));
+    }
+}
